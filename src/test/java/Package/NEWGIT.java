d7883a46f8cf0fe7bf4dbe9f40655bb9455bb81b
joymukherjee-678 <joymukherjee932@gmail.com>
2021-10-29 17:22:17 +0530

    committed

--- src/test/java/Package/NEWGIT.java
+++ src/test/java/Package/NEWGIT.java
@@ -3,7 +3,7 @@ package Package;
 public class NEWGIT {
 	public static void main(String[] args) {
 		System.out.println("joy");
-		System.out.println("mukherjee");
+		System.out.println(" mukherjee");
 	}
 
 }
